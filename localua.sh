41bd57f272b6e1cee5780cec1d413706a715aef2
Pierre Chapuis <git@catwell.info>
2024-06-26 10:57:55 +0200

    Lua 5.4.7

--- localua.sh
+++ localua.sh
@@ -6,7 +6,7 @@
 # Latest stable version available at: https://loadk.com/localua.sh
 # Maintained at: https://github.com/oploadk/localua
 
-DEFAULT_LUA_V="5.4.6"
+DEFAULT_LUA_V="5.4.7"
 DEFAULT_LR_V="3.11.1"
 
 usage () {
