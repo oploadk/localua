4f780293059f6a32147a32a60d4012c1304a610d
Pierre Chapuis <git@catwell.info>
2025-09-19 18:18:12 +0200

    luarocks 3.12.2

--- localua.sh
+++ localua.sh
@@ -7,7 +7,7 @@
 # Maintained at: https://github.com/oploadk/localua
 
 DEFAULT_LUA_V="5.4.8"
-DEFAULT_LR_V="3.12.0"
+DEFAULT_LR_V="3.12.2"
 
 usage () {
     >&2 echo -e "USAGE: $0 output-dir [lua-version] [luarocks-version]\n"
